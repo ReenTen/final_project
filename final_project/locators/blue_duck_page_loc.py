291c8e9be0766fdf06b246c7fd3db1dec5540a31
ReenTen <meleshko.devilhunter@gmail.com>
2022-08-18 10:25:27 +0300

    add locators for blue_duck_page.py

---
+++ final_project/locators/blue_duck_page_loc.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+
+class BlueDuckPageLoc:
+    blue_duck_page_url_loc = 'http://localhost/litecart/en/rubber-ducks-c-1/blue-duck-p-4'
+    add_to_cart_button_loc = (By.XPATH, '//button[@name="add_cart_product"]')
+
